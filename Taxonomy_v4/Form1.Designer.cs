d4b939e9598feae3d37144c2ba206ddfb326b456
alexmokshin <mokshin.alex@gmail.com>
2018-03-01 00:31:29 +0500

    Добавьте файлы проекта.

---
+++ Taxonomy_v4/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Taxonomy_v4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bt_openExcel = new System.Windows.Forms.Button();
+            this.dtGrid = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.действияToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.нормировкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.таксономияToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dtGrid)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // bt_openExcel
+            // 
+            this.bt_openExcel.Location = new System.Drawing.Point(12, 26);
+            this.bt_openExcel.Name = "bt_openExcel";
+            this.bt_openExcel.Size = new System.Drawing.Size(175, 23);
+            this.bt_openExcel.TabIndex = 0;
+            this.bt_openExcel.Text = "Открыть Excel";
+            this.bt_openExcel.UseVisualStyleBackColor = true;
+            this.bt_openExcel.Click += new System.EventHandler(this.Bt_openExcel_Click);
+            // 
+            // dtGrid
+            // 
+            this.dtGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtGrid.Location = new System.Drawing.Point(13, 55);
+            this.dtGrid.Name = "dtGrid";
+            this.dtGrid.Size = new System.Drawing.Size(985, 442);
+            this.dtGrid.TabIndex = 1;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.действияToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1046, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.выходToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(108, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.ВыходToolStripMenuItem_Click);
+            // 
+            // действияToolStripMenuItem
+            // 
+            this.действияToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.нормировкаToolStripMenuItem,
+            this.таксономияToolStripMenuItem});
+            this.действияToolStripMenuItem.Name = "действияToolStripMenuItem";
+            this.действияToolStripMenuItem.Size = new System.Drawing.Size(70, 20);
+            this.действияToolStripMenuItem.Text = "Действия";
+            // 
+            // нормировкаToolStripMenuItem
+            // 
+            this.нормировкаToolStripMenuItem.Name = "нормировкаToolStripMenuItem";
+            this.нормировкаToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.нормировкаToolStripMenuItem.Text = "Нормировка";
+            this.нормировкаToolStripMenuItem.Click += new System.EventHandler(this.НормировкаToolStripMenuItem_Click);
+            // 
+            // таксономияToolStripMenuItem
+            // 
+            this.таксономияToolStripMenuItem.Name = "таксономияToolStripMenuItem";
+            this.таксономияToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.таксономияToolStripMenuItem.Text = "Таксономия";
+            this.таксономияToolStripMenuItem.Click += new System.EventHandler(this.ТаксономияToolStripMenuItem_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(13, 504);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(745, 155);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1046, 707);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.dtGrid);
+            this.Controls.Add(this.bt_openExcel);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dtGrid)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt_openExcel;
+        private System.Windows.Forms.DataGridView dtGrid;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem действияToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem нормировкаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem таксономияToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Taxonomy_v4/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+using Microsoft.VisualBasic;
+
+namespace Taxonomy_v4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Bt_openExcel_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openfile = new OpenFileDialog
+            {
+                DefaultExt = ".xlsx",
+                Filter = "(.xlsx)|*.xlsx"
+            };
+            openfile.ShowDialog();
+
+            Microsoft.Office.Interop.Excel.Application excelApp = new Microsoft.Office.Interop.Excel.Application();
+            Workbook excelBook = excelApp.Workbooks.Open(
+                openfile.FileName.ToString(), 0, true, 5, "", "", true,
+                XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+            Worksheet excelSheet =
+                (Worksheet)excelBook.Worksheets.get_Item(1);
+            Range excelRange = excelSheet.UsedRange;
+            string strCellData = "";
+            int RowIndex = excelApp.Cells.SpecialCells(Microsoft.Office.Interop.Excel.XlCellType.xlCellTypeLastCell).Row;
+            int ColumnIndex = excelApp.Cells.SpecialCells(Microsoft.Office.Interop.Excel.XlCellType.xlCellTypeLastCell).Column;
+            System.Data.DataTable dt = new System.Data.DataTable();
+            for (ColumnIndex = 1; ColumnIndex <= excelRange.Columns.Count; ColumnIndex++)
+            {
+                string strColumn = "";
+                var range = excelRange.Cells[1, ColumnIndex] as Range;
+                if (range != null)
+                    if (range.Value2 != null) strColumn = range.Value2.ToString();
+                dt.Columns.Add(strColumn, typeof(string));
+            }
+            for (RowIndex = 1; RowIndex <= excelRange.Rows.Count; RowIndex++)
+            {
+                string strData = "";
+                for (ColumnIndex = 1; ColumnIndex <= excelRange.Columns.Count; ColumnIndex++)
+                {
+                    var range = excelRange.Cells[RowIndex, ColumnIndex] as Range;
+                    if (range != null)
+                    {
+                        strCellData = range.Value2 != null ? range.Value2.ToString() : String.Empty;
+                        strData += strCellData + "|";
+                    }
+
+                }
+                strData = strData.Remove(strData.Length - 1, 1);
+
+                if (!string.IsNullOrWhiteSpace(strData)) dt.Rows.Add(strData.Split('|'));
+            }
+            dtGrid.DataSource = dt.DefaultView;
+            dt.Rows[0].Delete();
+            excelBook.Close(true, null, null);
+            excelApp.Quit();
+        }
+
+        private void ВыходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void НормировкаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var list = new List<double>();
+            int rowcnt = dtGrid.Rows.Count;
+            for (int j = 0; j < dtGrid.Columns.Count; j++)
+            {
+                for (int i = 0; i < rowcnt; i++)
+                {
+                    list.Add(Convert.ToDouble(dtGrid[j, i].Value));
+                }
+                var max = list.Max();
+                var min = list.Min();
+                var listAfterNorm = new List<double>();
+                if (max == min)
+                    listAfterNorm.AddRange(Enumerable.Range(0, list.Count).Select(_ => min == 0 ? 0.0 : 1.0));
+                else
+                    listAfterNorm.AddRange(list.Select(t => Math.Round(((t - min) / (max - min)), 3)));
+                for (int i = 0; i < listAfterNorm.Count; i++)
+                {
+                    dtGrid[j, i].Value = listAfterNorm[i].ToString();
+                }
+                list.Clear();
+                listAfterNorm.Clear();
+
+            }
+        }
+
+        private void ТаксономияToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            double porog = Convert.ToDouble(Interaction.InputBox("Введите порог", "Ввод порога", "0,5", 100, 100));
+            int num_elm_tax = 0;
+            List<double> list_taxon1 = new List<double>();
+            List<double> list_taxon2 = new List<double>();
+            int taxonCnt = 1;
+            richTextBox1.Text = "Таксон " + taxonCnt + ":";
+            for (int j = 1; j < dtGrid.ColumnCount - 1; j++)
+            {
+
+                for (int i = 0; i < dtGrid.RowCount - 1; i++)
+                    if (j + 1 != dtGrid.ColumnCount)
+                    {
+                        if (dtGrid.Rows[i].Cells[j].Value != null)
+                            list_taxon1.Add(double.Parse(dtGrid.Rows[i].Cells[j].Value.ToString()));
+                        if (dtGrid.Rows[i].Cells[j + 1].Value != null)
+                            list_taxon2.Add(double.Parse(dtGrid.Rows[i].Cells[j + 1].Value.ToString()));
+                    }
+
+
+                if (GetRadius(list_taxon1, list_taxon2) <= porog)
+                {
+                    num_elm_tax++;
+                    richTextBox1.AppendText(dtGrid.Columns[j + 1].Name.ToString() + ",");
+                }
+                else
+                {
+                    taxonCnt++;
+                    num_elm_tax = 0;
+                    richTextBox1.AppendText(dtGrid.Columns[j + 1].Name.ToString() + Environment.NewLine + "Таксон " + taxonCnt + ":");
+                }
+                list_taxon1.Clear();
+                list_taxon2.Clear();
+            }
+
+        }
+
+        private double GetRadius(List<double> l1, List<double> l2)
+        {
+            double radius = 0;
+            for (int i = 0; i < l1.Count; i++)
+                radius = radius + Math.Pow((l1[i] - l2[i]), 2);
+            return Math.Sqrt(radius);
+        }
+    }
+}
